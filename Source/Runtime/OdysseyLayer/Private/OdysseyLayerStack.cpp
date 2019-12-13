6858f85e9ceee167cfd4b4c4e57d1c6d11111bd7
mike-praxinos <54707550+mike-praxinos@users.noreply.github.com>
2019-12-13 09:03:32 +0100

    docs: update copyright docs: update version name

--- Source/Runtime/OdysseyLayer/Private/OdysseyLayerStack.cpp
+++ Source/Runtime/OdysseyLayer/Private/OdysseyLayerStack.cpp
@@ -1,4 +1,5 @@
-// Copyright 2018-2019 Praxinos, Inc. All Rights Reserved.
+// Copyright © 2018-2019 Praxinos, Inc. All Rights Reserved.
+// IDDN FR.001.250001.002.S.P.2019.000.00000
 
 
 #include "OdysseyLayerStack.h"
