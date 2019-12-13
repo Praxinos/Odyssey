894d0e926981d270491ae40acca9ef13dd90bb42
mike-praxinos <54707550+mike-praxinos@users.noreply.github.com>
2019-12-13 12:42:45 +0100

    docs: update copyright

--- Source/Editor/OdysseyBrushEditor/Private/ODFunctionDragDropAction.cpp
+++ Source/Editor/OdysseyBrushEditor/Private/ODFunctionDragDropAction.cpp
@@ -1,5 +1,6 @@
-// Copyright 2018-2019 Praxinos, Inc. All Rights Reserved.
+// Copyright © 2018-2019 Praxinos, Inc. All Rights Reserved.
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+// IDDN FR.001.250001.002.S.P.2019.000.00000
 
 #include "ODFunctionDragDropAction.h"
 #include "EdGraph/EdGraph.h"
