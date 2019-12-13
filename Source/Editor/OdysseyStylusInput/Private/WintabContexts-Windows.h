3181d962611a6bfd7e6b8385a7d641fc84099818
mike-praxinos <54707550+mike-praxinos@users.noreply.github.com>
2019-12-13 08:51:07 +0100

    docs: update copyright

--- Source/Editor/OdysseyStylusInput/Private/WintabContexts-Windows.h
+++ Source/Editor/OdysseyStylusInput/Private/WintabContexts-Windows.h
@@ -1,4 +1,5 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright © 2018-2019 Praxinos, Inc. All Rights Reserved.
+// IDDN FR.001.250001.002.S.P.2019.000.00000
 
 #pragma once
 
