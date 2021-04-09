2995f3d6f90ba9f7f2bc75c02f2a8f34bee568aa
mike-praxinos <54707550+mike-praxinos@users.noreply.github.com>
2021-04-09 09:40:43 +0200

    feat: add an intermediate (master) material instance for all plane materials

--- Source/EposSequenceEditor/Private/ShotHelpers/ShotSequenceHelpers_Camera.cpp
+++ Source/EposSequenceEditor/Private/ShotHelpers/ShotSequenceHelpers_Camera.cpp
@@ -3,6 +3,7 @@
 
 #include "ShotSequenceHelpers.h"
 
+#include "AssetRegistryModule.h"
 #include "AssetToolsModule.h"
 #include "Channels/MovieSceneFloatChannel.h"
 #include "CineCameraActor.h"
@@ -322,23 +323,77 @@ ShotSequenceHelpers::ComputePlaneScale( const ACineCameraActor* iCamera, float i
 
 static
 UMaterialInstanceConstant*
-CreateMaterialInstanceConstantAsset( UMovieSceneSequence* iSequence, FString& oPackageName, FString& oAssetName )
+CreateMaterialInstanceConstantAsset( UMovieSceneSequence* iSequence, UMovieSceneSequence* iRootSequence, FString& oPackageName, FString& oAssetName )
 {
-    UMaterial* material = LoadObject<UMaterial>( nullptr, TEXT( "/Epos/M_Plane_Basic.M_Plane_Basic" ) );
-    if( !material )
+    UMaterial* material_root = LoadObject<UMaterial>( nullptr, TEXT( "/Epos/M_Plane_Root.M_Plane_Root" ) );
+    if( !material_root )
         return nullptr;
+
+    FAssetToolsModule& assetToolsModule = FModuleManager::GetModuleChecked<FAssetToolsModule>( "AssetTools" );
+    FAssetRegistryModule& assetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>( TEXT( "AssetRegistry" ) );
+
+    UMaterialInstanceConstant* material_master = nullptr;
+
+    UObject* root_asset = iRootSequence->GetMovieScene()->GetOuter();
+    UPackage* root_package = root_asset->GetPackage();
+    FString root_package_name = root_package->GetName() / "MI_Plane_Master"; // ie. /Game/MyStoryboard2
+
+    TArray<FAssetData> asset_datas;
+    assetRegistryModule.Get().GetAssetsByPackageName( *root_package_name, asset_datas );
+    if( !asset_datas.Num() )
+    {
+        FString package_name;
+        FString asset_name;
+        assetToolsModule.Get().CreateUniqueAssetName( root_package_name, "", package_name, asset_name );
+
+        UMaterialInstanceConstantFactoryNew* factory = NewObject<UMaterialInstanceConstantFactoryNew>();
+        factory->InitialParent = material_root;
+
+        FString package_path = FPackageName::GetLongPackagePath( package_name );
+        UObject* new_object = assetToolsModule.Get().CreateAsset( asset_name, package_path, UMaterialInstanceConstant::StaticClass(), factory );
+
+        material_master = Cast<UMaterialInstanceConstant>( new_object );
+
+        //---
+
+        FStaticParameterSet static_params;
+        material_master->GetStaticParameterValues( static_params );
+        for( auto& parameter : static_params.StaticSwitchParameters )
+            parameter.bOverride = true;
+        material_master->UpdateStaticPermutation( static_params );
+
+        // Needed to compute all cases during creation, to have all shaders computed
+        for( int combination = 0; combination < FMath::Pow( 2, static_params.StaticSwitchParameters.Num() ); combination++ )
+        {
+            //UE_LOG( LogTemp, Warning, TEXT( "combination: %d" ), combination );
+            for( int i = 0; i < static_params.StaticSwitchParameters.Num(); i++ )
+            {
+                static_params.StaticSwitchParameters[i].Value = combination & ( 1 << i );
+                //UE_LOG( LogTemp, Warning, TEXT( "i: %d - value: %d" ), i, static_params.StaticSwitchParameters[i].Value );
+                material_master->UpdateStaticPermutation( static_params );
+            }
+        }
+    }
+    else
+    {
+        material_master = Cast<UMaterialInstanceConstant>( asset_datas[0].GetAsset() );
+    }
+
+    if( !material_master )
+        return nullptr;
+
+    //---
 
     UPackage* package = iSequence->GetPackage();
     FString package_name = package->GetName(); // ie. /Game/MyStoryboard2/shot0001_01
 
-    FAssetToolsModule& Module = FModuleManager::GetModuleChecked<FAssetToolsModule>( "AssetTools" );
-    Module.Get().CreateUniqueAssetName( package_name, "_Inst", oPackageName, oAssetName );
+    assetToolsModule.Get().CreateUniqueAssetName( package_name, "_MI_01", oPackageName, oAssetName );
 
     UMaterialInstanceConstantFactoryNew* factory = NewObject<UMaterialInstanceConstantFactoryNew>();
-    factory->InitialParent = material;
+    factory->InitialParent = material_master;
 
     FString package_path = FPackageName::GetLongPackagePath( oPackageName );
-    UObject* new_object = Module.Get().CreateAsset( oAssetName, package_path, UMaterialInstanceConstant::StaticClass(), factory );
+    UObject* new_object = assetToolsModule.Get().CreateAsset( oAssetName, package_path, UMaterialInstanceConstant::StaticClass(), factory );
 
     return Cast<UMaterialInstanceConstant>( new_object );
 }
@@ -355,7 +410,7 @@ CreateTexture2DAsset( UMovieSceneSequence* iSequence, UMaterialInterface* iMater
     FString package_name = package->GetName(); // ie. /Game/MyStoryboard2/M_Plane_Basic_Inst
 
     FAssetToolsModule& Module = FModuleManager::GetModuleChecked<FAssetToolsModule>( "AssetTools" );
-    Module.Get().CreateUniqueAssetName( package_name, "_Texture", oPackageName, oAssetName );
+    Module.Get().CreateUniqueAssetName( package_name, "_T_01", oPackageName, oAssetName );
 
     FString package_path = FPackageName::GetLongPackagePath( oPackageName );
     UObject* new_object = Module.Get().DuplicateAsset( oAssetName, package_path, texture_transparent );
@@ -392,7 +447,7 @@ ShotSequenceHelpers::SpawnAndBindPlane( ISequencer& iSequencer, UMovieSceneSeque
 
     FString new_material_package_name;
     FString new_material_asset_name;
-    UMaterialInstanceConstant* new_material = CreateMaterialInstanceConstantAsset( iSequence, new_material_package_name, new_material_asset_name );
+    UMaterialInstanceConstant* new_material = CreateMaterialInstanceConstantAsset( iSequence, iSequencer.GetRootMovieSceneSequence(), new_material_package_name, new_material_asset_name );
     if( !new_material )
         return;
 
@@ -403,24 +458,6 @@ ShotSequenceHelpers::SpawnAndBindPlane( ISequencer& iSequencer, UMovieSceneSeque
         return;
 
     new_material->SetTextureParameterValueEditorOnly( TEXT( "DrawingTexture" ), new_texture );
-
-    FStaticParameterSet static_params;
-    new_material->GetStaticParameterValues( static_params );
-    for( auto& parameter : static_params.StaticSwitchParameters )
-        parameter.bOverride = true;
-    new_material->UpdateStaticPermutation( static_params );
-
-    // Needed to compute all cases during creation, to have all shaders computed
-    for( int combination = 0; combination < FMath::Pow( 2, static_params.StaticSwitchParameters.Num() ); combination++ )
-    {
-        //UE_LOG( LogTemp, Warning, TEXT( "combination: %d" ), combination );
-        for( int i = 0; i < static_params.StaticSwitchParameters.Num(); i++ )
-        {
-            static_params.StaticSwitchParameters[i].Value = combination & ( 1 << i );
-            //UE_LOG( LogTemp, Warning, TEXT( "i: %d - value: %d" ), i, static_params.StaticSwitchParameters[i].Value );
-            new_material->UpdateStaticPermutation( static_params );
-        }
-    }
 
     //---
 
