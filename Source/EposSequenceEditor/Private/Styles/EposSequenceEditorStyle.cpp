ef62099e9a9fc797eca36b2f2650acfc917c7ddf
mike-praxinos <54707550+mike-praxinos@users.noreply.github.com>
2021-07-20 17:16:35 +0200

    chore: add some svg icons

--- Source/EposSequenceEditor/Private/Styles/EposSequenceEditorStyle.cpp
+++ Source/EposSequenceEditor/Private/Styles/EposSequenceEditorStyle.cpp
@@ -69,16 +69,16 @@ FEposSequenceEditorStyle::FEposSequenceEditorStyle()
     //---
 
     Set( "EposSequenceEditor.Settings", new IMAGE_BRUSH( "epos-settings-24x", Icon48x48 ) );
-    Set( "EposSequenceEditor.Settings.Small", new IMAGE_BRUSH( "epos-settings-24x", Icon24x24 ) );
-    Set( "EposSequenceEditor.Help", new IMAGE_BRUSH( "epos-help-24x", Icon48x48 ) );
-    Set( "EposSequenceEditor.Help.Small", new IMAGE_BRUSH( "epos-help-24x", Icon24x24 ) );
-    Set( "EposSequenceEditor.OpenAboutWindow", new IMAGE_BRUSH( "epos-about-24x", Icon48x48 ) );
-    Set( "EposSequenceEditor.OpenAboutWindow.Small", new IMAGE_BRUSH( "epos-about-24x", Icon24x24 ) );
-
+    Set( "EposSequenceEditor.Settings.Small", new IMAGE_BRUSH( "epos-settings-24x", FVector2D( 20.f, 20.f ) ) );
     Set( "EposSequenceEditor.OpenSequenceEditorSettings", new IMAGE_BRUSH( "epos-settings-24x", Icon48x48 ) );
     Set( "EposSequenceEditor.OpenSequenceEditorSettings.Small", new IMAGE_BRUSH( "epos-settings-24x", Icon24x24 ) );
     Set( "EposSequenceEditor.OpenTrackEditorSettings", new IMAGE_BRUSH( "epos-settings-24x", Icon48x48 ) );
     Set( "EposSequenceEditor.OpenTrackEditorSettings.Small", new IMAGE_BRUSH( "epos-settings-24x", Icon24x24 ) );
+
+    Set( "EposSequenceEditor.Help", new IMAGE_BRUSH( "epos-help-24x", Icon48x48 ) );
+    Set( "EposSequenceEditor.Help.Small", new IMAGE_BRUSH( "epos-help-24x", FVector2D( 20.f, 20.f ) ) );
+    Set( "EposSequenceEditor.OpenAboutWindow", new IMAGE_BRUSH( "epos-about-24x", Icon48x48 ) );
+    Set( "EposSequenceEditor.OpenAboutWindow.Small", new IMAGE_BRUSH( "epos-about-24x", Icon24x24 ) );
 
     Set( "About.Facebook", new IMAGE_BRUSH( "About/facebook", FVector2D( 30, 30 ) ) );
     Set( "About.LinkedIn", new IMAGE_BRUSH( "About/linkedin", FVector2D( 30, 30 ) ) );
