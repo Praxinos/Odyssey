3c57d53ae3af556d2dbe38299d7b6c9de173fb88
mike-praxinos <54707550+mike-praxinos@users.noreply.github.com>
2021-12-03 10:37:58 +0100

    feat: add naming convention module

---
+++ Source/EposNamingConvention/EposNamingConvention.Build.cs
@@ -0,0 +1,53 @@
+// IDDN.FR.001.220036.000.S.P.2021.000.00000
+// EPOS is subject to copyright © laws and is the legal and intellectual property of Praxinos,Inc
+
+using System.IO;
+using UnrealBuildTool;
+
+public class EposNamingConvention : ModuleRules
+{
+    public EposNamingConvention(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicIncludePaths.AddRange(
+            new string[] {
+                // ... add public include paths required here ...
+            }
+            );
+
+        PrivateIncludePaths.AddRange(
+            new string[] {
+                // ... add other private include paths required here ...
+            }
+            );
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                // ... add other public dependencies that you statically link with here ...
+            }
+            );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                // [RUNTIME]
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                // [RUNTIME] custom engine dependencies
+                // [RUNTIME] custom plugin dependencies
+            }
+            );
+
+        DynamicallyLoadedModuleNames.AddRange(
+            new string[]
+            {
+                // ... add any modules that your module loads dynamically here ...
+            }
+            );
+    }
+}
