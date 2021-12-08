2f4f714690b641f7aabeb6e2844f737c41f49111
mike-praxinos <54707550+mike-praxinos@users.noreply.github.com>
2021-12-08 14:39:04 +0100

    refactor: improve renaming path/name variables

--- Source/EposNamingConvention/Private/EposNamingConvention.cpp
+++ Source/EposNamingConvention/Private/EposNamingConvention.cpp
@@ -17,6 +17,22 @@
 
 //static
 FString
+NamingConvention::GetRootPath( const UMovieSceneSequence* iRootSequence )
+{
+    return iRootSequence->GetPackage()->GetName(); // ie. /Game/MyStoryboard2
+}
+
+//static
+FString
+NamingConvention::GetMasterPath( const UMovieSceneSequence* iRootSequence )
+{
+    return GetRootPath( iRootSequence ) / TEXT( "Master" ); // ie. /Game/MyStoryboard2/Master
+}
+
+//---
+
+//static
+FString
 NamingConvention::GenerateCameraActorPathName( const IMovieScenePlayer& iSequencer, const UMovieSceneSequence* iRootSequence, const UMovieSceneSequence* iSequence, FString& oPath, FString& oName )
 {
     oPath = FPaths::GetBaseFilename( iRootSequence->GetPathName() );
@@ -59,9 +75,7 @@ NamingConvention::GeneratePlaneTrackName( const IMovieScenePlayer& iSequencer, c
 FString
 NamingConvention::GetMasterMaterialPathName( const UMovieSceneSequence* iRootSequence, FString& oPath, FString& oName )
 {
-    UPackage* root_package = iRootSequence->GetPackage();
-
-    oPath = root_package->GetName() / TEXT( "Master" );
+    oPath = GetMasterPath( iRootSequence );
     oName = TEXT( "MI_Plane" );
 
     return oPath / oName;
@@ -71,9 +85,7 @@ NamingConvention::GetMasterMaterialPathName( const UMovieSceneSequence* iRootSeq
 FString
 NamingConvention::GetMasterTexturePathName( const UMovieSceneSequence* iRootSequence, FString& oPath, FString& oName )
 {
-    UPackage* root_package = iRootSequence->GetPackage();
-
-    oPath = root_package->GetName() / TEXT( "Master" );
+    oPath = GetMasterPath( iRootSequence );
     oName = TEXT( "T_Transparent" );
 
     return oPath / oName;
@@ -85,13 +97,16 @@ NamingConvention::GetMasterTexturePathName( const UMovieSceneSequence* iRootSequ
 FString
 NamingConvention::GenerateNoteAssetPathName( const UMovieSceneSequence* iRootSequence, const UMovieSceneSequence* iSequence, FString& oPath, FString& oName )
 {
-    FString root_package_pathname = iRootSequence->GetPackage()->GetName(); // ie. /Game/MyStoryboard2
-    FString current_package_name = FPaths::GetBaseFilename( iSequence->GetPackage()->GetName() ); // ie. shot_0002_01
+    FString root_path = GetRootPath( iRootSequence ); // ie. /Game/MyStoryboard2
+    FString current_sequence_name = FPackageName::GetShortName( iSequence->GetPackage() ); // ie. shot_0002_01
+
+    FString note_pathname_base = root_path / TEXT( "Notes" ) / current_sequence_name;
+    FString note_suffix = TEXT( "_N_01" );
 
     FString note_pathname;
     FString note_name;
     FAssetToolsModule& assetToolsModule = FModuleManager::GetModuleChecked<FAssetToolsModule>( "AssetTools" );
-    assetToolsModule.Get().CreateUniqueAssetName( root_package_pathname / TEXT( "Notes" ) / current_package_name, TEXT( "_N_01" ), note_pathname, note_name );
+    assetToolsModule.Get().CreateUniqueAssetName( note_pathname_base, note_suffix, note_pathname, note_name );
 
     oName = note_name;
     oPath = FPackageName::GetLongPackagePath( note_pathname );
@@ -103,13 +118,16 @@ NamingConvention::GenerateNoteAssetPathName( const UMovieSceneSequence* iRootSeq
 FString
 NamingConvention::GenerateMaterialAssetPathName( const UMovieSceneSequence* iRootSequence, const UMovieSceneSequence* iSequence, FString& oPath, FString& oName )
 {
-    UPackage* package = iSequence->GetPackage();
-    FString current_package_pathname = package->GetName(); // ie. /Game/MyStoryboard2/shot0001_01
+    FString root_path = GetRootPath( iRootSequence ); // ie. /Game/MyStoryboard2
+    FString current_sequence_name = FPackageName::GetShortName( iSequence->GetPackage() ); // ie. shot_0002_01
+
+    FString material_pathname_base = root_path / current_sequence_name;
+    FString material_suffix = TEXT( "_MI_01" );
 
     FString material_pathname;
     FString material_name;
     FAssetToolsModule& assetToolsModule = FModuleManager::GetModuleChecked<FAssetToolsModule>( "AssetTools" );
-    assetToolsModule.Get().CreateUniqueAssetName( current_package_pathname, TEXT( "_MI_01" ), material_pathname, material_name );
+    assetToolsModule.Get().CreateUniqueAssetName( material_pathname_base, material_suffix, material_pathname, material_name );
 
     oName = material_name;
     oPath = FPackageName::GetLongPackagePath( material_pathname );
@@ -121,13 +139,16 @@ NamingConvention::GenerateMaterialAssetPathName( const UMovieSceneSequence* iRoo
 FString
 NamingConvention::GenerateTextureAssetPathName( const UMovieSceneSequence* iRootSequence, const UMovieSceneSequence* iSequence, UMaterialInterface* iMaterial, FString& oPath, FString& oName )
 {
-    UPackage* package = iMaterial->GetPackage();
-    FString current_package_pathname = package->GetName(); // ie. /Game/MyStoryboard2/M_Plane_Basic_Inst
+    FString root_path = GetRootPath( iRootSequence ); // ie. /Game/MyStoryboard2
+    FString current_material_name = FPackageName::GetShortName( iMaterial->GetPackage() ); // ie. shot_0002_01_MI_01
+
+    FString texture_pathname_base = root_path / current_material_name;
+    FString texture_suffix = TEXT( "_T_01" );
 
     FString texture_pathname;
     FString texture_name;
     FAssetToolsModule& assetToolsModule = FModuleManager::GetModuleChecked<FAssetToolsModule>( "AssetTools" );
-    assetToolsModule.Get().CreateUniqueAssetName( current_package_pathname, TEXT( "_T_01" ), texture_pathname, texture_name );
+    assetToolsModule.Get().CreateUniqueAssetName( texture_pathname_base, texture_suffix, texture_pathname, texture_name );
 
     oName = texture_name;
     oPath = FPackageName::GetLongPackagePath( texture_pathname );
--- Source/EposNamingConvention/Public/EposNamingConvention.h
+++ Source/EposNamingConvention/Public/EposNamingConvention.h
@@ -28,4 +28,8 @@ public:
 
     static FString GenerateMaterialAssetPathName( const UMovieSceneSequence* iRootSequence, const UMovieSceneSequence* iSequence, FString& oPath, FString& oName );
     static FString GenerateTextureAssetPathName( const UMovieSceneSequence* iRootSequence, const UMovieSceneSequence* iSequence, UMaterialInterface* iMaterial, FString& oPath, FString& oName );
+
+private:
+    static FString GetRootPath( const UMovieSceneSequence* iRootSequence );
+    static FString GetMasterPath( const UMovieSceneSequence* iRootSequence );
 };
