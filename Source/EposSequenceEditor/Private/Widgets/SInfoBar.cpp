3837a28f5ab939a5256f53ddebd07c12e802582c
mike-praxinos <54707550+mike-praxinos@users.noreply.github.com>
2022-04-19 10:32:15 +0200

    chore: update iddn to IDDN.FR.001.220036.001.S.P.2021.000.00000

--- Source/EposSequenceEditor/Private/Widgets/SInfoBar.cpp
+++ Source/EposSequenceEditor/Private/Widgets/SInfoBar.cpp
@@ -1,4 +1,4 @@
-// IDDN.FR.001.220036.000.S.P.2021.000.00000
+// IDDN.FR.001.220036.001.S.P.2021.000.00000
 // EPOS is subject to copyright © laws and is the legal and intellectual property of Praxinos,Inc
 
 #include "Widgets/SInfoBar.h"
--- build-package.py
+++ build-package.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# IDDN.FR.001.220036.000.S.P.2021.000.00000
+# IDDN.FR.001.220036.001.S.P.2021.000.00000
 # EPOS is subject to copyright © laws and is the legal and intellectual property of Praxinos,Inc
 
 import argparse
