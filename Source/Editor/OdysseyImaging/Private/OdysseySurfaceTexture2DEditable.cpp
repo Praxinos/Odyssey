a79cc3c8ee9b240dc4d2ed453b0bc0076f585f93
thomas-praxinos <thomas.schmitt@praxinos.coop>
2022-05-05 09:17:07 +0200

    fix: crash on creating new textures

--- Source/Editor/OdysseyImaging/Private/OdysseySurfaceTexture2DEditable.cpp
+++ Source/Editor/OdysseyImaging/Private/OdysseySurfaceTexture2DEditable.cpp
@@ -128,18 +128,11 @@ InitTextureWithBlockData(const ::ULIS::FBlock* iBlock, UTexture2D* iTexture, ETe
         TArray64<uint8> dst;
         dst.SetNumUninitialized(iBlock->Width() * iBlock->Height() * TextureSourceFormatBytesPerPixel(iFormat));
         ConvertULISFormatToTextureSourceFormat(block->Bits(), dst.GetData(), iBlock->Width(), iBlock->Height(), iFormat);
-        uint8* mip = iTexture->Source.LockMip(0);
-        memcpy(mip, dst.GetData(), dst.Num());
-        iTexture->Source.UnlockMip(0);
-        iTexture->FinishCachePlatformData();
+        iTexture->Source.Init(iBlock->Width(), iBlock->Height(), 1, 1, iFormat, dst.GetData());
     }
     else
     {
-        uint8* mip = iTexture->Source.LockMip(0);
-        memcpy(mip, block->Bits(), block->BytesTotal());
-        iTexture->Source.UnlockMip(0);
-        iTexture->Source.Compress();
-        iTexture->FinishCachePlatformData();
+        iTexture->Source.Init(block->Width(), block->Height(), 1, 1, iFormat, block->Bits());
     }
 
     if (block != iBlock)
