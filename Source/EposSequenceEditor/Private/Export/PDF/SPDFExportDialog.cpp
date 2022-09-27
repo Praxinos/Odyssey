5d73c91f62ce206b05ba287dcc8ea9ed6df82af0
mike-praxinos <54707550+mike-praxinos@users.noreply.github.com>
2022-09-27 14:55:07 +0200

    doc: remove comments

--- Source/EposSequenceEditor/Private/Export/PDF/SPDFExportDialog.cpp
+++ Source/EposSequenceEditor/Private/Export/PDF/SPDFExportDialog.cpp
@@ -6,13 +6,10 @@
 #include "AssetRegistry/AssetData.h"
 #include "AssetRegistry/AssetRegistryModule.h"
 #include "AssetToolsModule.h"
-#include "Async/Async.h"
 #include "EditorStyleSet.h"
 #include "Factories/Factory.h"
 #include "FrameNumberDetailsCustomization.h"
 #include "Framework/Docking/TabManager.h"
-#include "HAL/PlatformProcess.h"
-#include "HAL/Runnable.h"
 #include "IAssetTools.h"
 #include "IDetailsView.h"
 #include "ImageWrapperHelper.h"
@@ -27,7 +24,6 @@
 #include "SPrimaryButton.h"
 #include "Subsystems/AssetEditorSubsystem.h"
 #include "Widgets/Docking/SDockTab.h"
-#include "Widgets/Images/SThrobber.h"
 #include "Widgets/Input/SButton.h"
 #include "Widgets/Input/SSpinBox.h"
 #include "Widgets/Layout/SScrollBorder.h"
@@ -49,114 +45,6 @@
 #include "Settings/NamingConventionSettings.h"
 
 #define LOCTEXT_NAMESPACE "PDFExportDialog"
-
-//---
-
-DECLARE_DELEGATE_OneParam( FOnConstructPDFLayoutComplete, UPDFDocExportWidget* );
-
-class FConstructPDFLayoutRunnable
-    : public FRunnable
-{
-public:
-    FConstructPDFLayoutRunnable( UClass* iWidgetClass, FExportStruct iExportStruct );
-    virtual ~FConstructPDFLayoutRunnable();
-
-    void CreateAndStartThread();
-
-    // FRunnable functions
-    virtual bool Init() override;
-    virtual uint32 Run() override;
-    virtual void Stop() override;
-    virtual void Exit() override;
-    // FRunnable
-
-    FOnConstructPDFLayoutComplete OnCompleteDelegate;
-
-    friend class SExportPDFSettings;
-
-protected:
-    FRunnableThread* mThread { nullptr };
-
-    UClass* mWidgetClass { nullptr };
-    FExportStruct mExportStruct;
-
-    UPDFDocExportWidget* mWidget { nullptr };
-
-    //bool bStopThread = false;
-};
-
-//---
-
-FConstructPDFLayoutRunnable::FConstructPDFLayoutRunnable( UClass* iWidgetClass, FExportStruct iExportStruct )
-    : mWidgetClass( iWidgetClass )
-    , mExportStruct( iExportStruct )
-{
-}
-
-FConstructPDFLayoutRunnable::~FConstructPDFLayoutRunnable()
-{
-    if( mThread != nullptr )
-    {
-        mThread->Kill( true );
-        delete mThread;
-    }
-}
-
-void
-FConstructPDFLayoutRunnable::CreateAndStartThread()
-{
-    mThread = FRunnableThread::Create( this, TEXT( "ConstructPDFLayout" ) );
-}
-
-bool FConstructPDFLayoutRunnable::Init()
-{
-    // First thing to do is set the slate loading thread ID
-    // This guarantees all systems know that a slate thread exists
-    GSlateLoadingThreadId = FPlatformTLS::GetCurrentThreadId();
-
-    return true;
-}
-
-uint32 FConstructPDFLayoutRunnable::Run()
-{
-    FTaskTagScope Scope( ETaskTag::ESlateThread );
-
-    check( GSlateLoadingThreadId == FPlatformTLS::GetCurrentThreadId() );
-
-    //bStopThread = false;
-
-    if( mWidgetClass )
-    {
-        UWorld* world = GEditor->GetEditorWorldContext().World();
-        mWidget = CreateWidget<UPDFDocExportWidget>( world, mWidgetClass );
-        check( mWidget );
-
-        mWidget->OnConstructPDFLayout( mExportStruct, true );
-    }
-
-    OnCompleteDelegate.ExecuteIfBound( mWidget );
-    mWidget = nullptr;
-
-    // Tear down the slate loading thread ID
-    FPlatformAtomics::InterlockedExchange( (int32*)&GSlateLoadingThreadId, 0 );
-
-    // Return success
-    return 0;
-}
-
-
-void FConstructPDFLayoutRunnable::Exit()
-{
-    mWidgetClass = nullptr;
-    mWidget = nullptr;
-}
-
-
-void FConstructPDFLayoutRunnable::Stop()
-{
-    // Force our thread to stop early
-    //bStopThread = true;
-}
 
 //---
 
@@ -199,40 +87,9 @@ SExportPDFSettings::Construct( const FArguments& InArgs, TWeakPtr<ISequencer> iS
 
     //---
 
-    //FExportStruct image_sequence_struct;
-    //FExportConverter converter( mSequencer, mRootSequence, &mExportPDFSettings->Options.MarkSettings, &image_sequence_struct );
-
     UClass* pdf_doc_class = mExportPDFSettings->Options.PDFDocWidgetSoftClass.LoadSynchronous();
     if( pdf_doc_class )
     {
-        //FExportStruct image_sequence_struct;
-        //FExportConverter converter( mSequencer, mRootSequence, &mExportPDFSettings->Options.MarkSettings, &image_sequence_struct );
-
-        //TSharedPtr<FConstructPDFLayoutRunnable> runnable = MakeShared<FConstructPDFLayoutRunnable>( pdf_doc_class, image_sequence_struct );
-
-        //auto UpdateWidget = [this]( UPDFDocExportWidget* iWidget )
-        //{
-        //    mPDFSlot->DetachWidget();
-        //    mPDFDocWidget = nullptr;
-
-        //    mPDFDocWidget = iWidget;
-
-        //    mPDFSlot->AttachWidget(
-        //        SNew( SBox )
-        //        .MinAspectRatio( this, &SExportPDFSettings::GetPageRatio )
-        //        .MaxAspectRatio( this, &SExportPDFSettings::GetPageRatio )
-        //        [
-        //            mPDFDocWidget->TakeWidget()
-        //        ]
-        //    );
-        //};
-
-        //runnable->OnCompleteDelegate = FOnConstructPDFLayoutComplete::CreateLambda( UpdateWidget );
-
-        //runnable->CreateAndStartThread();
-
-        //---
-
         const FText ProgressText = LOCTEXT( "ConstructAndRenderPDFLayout", "Construct and Render PDF Layout..." );
         FScopedSlowTask Progress( 0, ProgressText );
         Progress.MakeDialog();
@@ -246,35 +103,6 @@ SExportPDFSettings::Construct( const FArguments& InArgs, TWeakPtr<ISequencer> iS
 
         mPDFDocWidget->OnConstructPDFLayout( image_sequence_struct, true );
     }
-
-    //TFuture<void> Result = Async( EAsyncExecution::TaskGraphMainThread,
-    //                              [this, pdf_doc_class, image_sequence_struct]()
-    //                              {
-    //                                  if( pdf_doc_class )
-    //                                  {
-    //                                      UWorld* world = GEditor->GetEditorWorldContext().World();
-    //                                      mPDFDocWidget = CreateWidget<UPDFDocExportWidget>( world, pdf_doc_class );
-    //                                      check( mPDFDocWidget );
-
-    //                                      mPDFDocWidget->OnConstructPDFLayout( image_sequence_struct, true );
-    //                                  }
-    //                              } );
-
-
-    //uint32 LastProcessed = 0;
-    //uint32 max_processed = 200;
-    //const FText ProgressText = LOCTEXT( "ConstructPDFLayout", "Construct PDF Layout {0}/{1} ..." );
-    //FScopedSlowTask Progress( max_processed, FText::Format( ProgressText, LastProcessed, max_processed ) );
-    //Progress.MakeDialog();
-
-    //while( !Result.WaitFor( FTimespan::FromMilliseconds( 33.0 ) ) )
-    //{
-    //    //uint32 LocalProcessed = Processed.Load( EMemoryOrder::Relaxed );
-    //    //Progress.EnterProgressFrame( LocalProcessed - LastProcessed, FText::Format( ProgressText, LocalProcessed, StaticMeshes.Num() ) );
-    //    LastProcessed++;
-    //    Progress.EnterProgressFrame( 1, FText::Format( ProgressText, LastProcessed, max_processed ) );
-    //    //LastProcessed = LocalProcessed;
-    //}
 
     //---
 
@@ -308,7 +136,6 @@ SExportPDFSettings::Construct( const FArguments& InArgs, TWeakPtr<ISequencer> iS
             ////.VAlign( VAlign_Fill )
             //.Expose( mPDFSlot )
             //[
-                //SNew( SThrobber )
                 SNew( SBox )
                 .MinAspectRatio( this, &SExportPDFSettings::GetPageRatio )
                 .MaxAspectRatio( this, &SExportPDFSettings::GetPageRatio )
@@ -365,70 +192,6 @@ SExportPDFSettings::GlobalSettingsChanged( const FPropertyChangedEvent& iEvent )
     mExportPDFSettings->SaveConfig();
 
     FSlateApplication::Get().DismissAllMenus();
-
-    //---
-
-    //FExportStruct image_sequence_struct;
-    //FExportConverter converter( mSequencer, mRootSequence, &mExportPDFSettings->Options.MarkSettings, &image_sequence_struct );
-
-    //UClass* pdf_doc_class = mExportPDFSettings->Options.PDFDocWidgetSoftClass.LoadSynchronous();
-
-    //                                  mPDFSlot->DetachWidget();
-    //                                  mPDFDocWidget = nullptr;
-
-    //                                  mPDFSlot->AttachWidget(
-    //                                      SNew( SThrobber )
-    //                                  );
-
-    //TFuture<void> Result = Async( EAsyncExecution::TaskGraphMainThread,
-    //                              [this, pdf_doc_class, image_sequence_struct]()
-    //                              {
-    //                                  ensure( IsInGameThread() );
-
-    //                                  //---
-
-    //                                  if( pdf_doc_class )
-    //                                  {
-    //                                      UWorld* world = GEditor->GetEditorWorldContext().World();
-    //                                      mPDFDocWidget = CreateWidget<UPDFDocExportWidget>( world, pdf_doc_class );
-    //                                      check( mPDFDocWidget );
-
-    //                                      mPDFDocWidget->OnConstructPDFLayout( image_sequence_struct, true );
-    //                                  }
-
-    //                                  //---
-
-    //                              } );
-
-    //FPlatformProcess::Sleep( 10 );
-
-    //ensure( IsInGameThread() );
-
-    //uint32 LastProcessed = 0;
-    //uint32 max_processed = 200;
-    //const FText ProgressText = LOCTEXT( "ConstructPDFLayout", "Construct PDF Layout {0}/{1} ..." );
-    //FScopedSlowTask Progress( 0, FText::Format( ProgressText, LastProcessed, max_processed ) );
-    //Progress.MakeDialog();
-
-    //while( !Result.WaitFor( FTimespan::FromMilliseconds( 33.0 ) ) )
-    //{
-    //    //uint32 LocalProcessed = Processed.Load( EMemoryOrder::Relaxed );
-    //    //Progress.EnterProgressFrame( LocalProcessed - LastProcessed, FText::Format( ProgressText, LocalProcessed, StaticMeshes.Num() ) );
-    //    LastProcessed++;
-    //    //Progress.EnterProgressFrame( 1, FText::Format( ProgressText, LastProcessed, max_processed ) );
-    //    //LastProcessed = LocalProcessed;
-    //}
-
-    //                                  mPDFSlot->DetachWidget();
-
-    //                                  mPDFSlot->AttachWidget(
-    //                                      SNew( SBox )
-    //                                      .MinAspectRatio( this, &SExportPDFSettings::GetPageRatio )
-    //                                      .MaxAspectRatio( this, &SExportPDFSettings::GetPageRatio )
-    //                                      [
-    //                                          mPDFDocWidget->TakeWidget()
-    //                                      ]
-    //                                  );
 
     //---
 
