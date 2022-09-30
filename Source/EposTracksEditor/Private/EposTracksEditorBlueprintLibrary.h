5600f3cd51019c866a0aeb89b0cd45f99ce0e9c0
mike-praxinos <54707550+mike-praxinos@users.noreply.github.com>
2022-09-30 16:26:31 +0200

    feat: expose tracks editor settings to blueprint

---
+++ Source/EposTracksEditor/Private/EposTracksEditorBlueprintLibrary.h
@@ -0,0 +1,28 @@
+// IDDN.FR.001.220036.001.S.P.2021.000.00000
+// EPOS is subject to copyright © laws and is the legal and intellectual property of Praxinos,Inc - Year of publishing 2022
+
+#pragma once
+
+//#include "LevelSequenceEditorBlueprintLibrary.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+
+#include "Settings/EposTracksEditorSettings.h"
+
+#include "EposTracksEditorBlueprintLibrary.generated.h"
+
+//---
+
+UCLASS()
+class EPOSTRACKSEDITOR_API UEposTracksEditorBlueprintLibrary
+    : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+
+    /**
+     * Get Settings
+     */
+    UFUNCTION( BlueprintCallable, Category = "Epos Tracks Editor" )
+    static UEposTracksEditorSettings* GetEposTracksEditorDefaultSettings();
+};
