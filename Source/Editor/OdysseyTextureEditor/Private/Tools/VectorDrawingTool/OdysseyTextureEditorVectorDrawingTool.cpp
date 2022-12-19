4a0fcbf960cc15d9ebab570824dda2a1311d79ad
Gary-Praxinos <gary.gabriel@praxinos.coop>
2022-12-19 17:42:28 +0100

    wip: vector path edit tool

---
+++ Source/Editor/OdysseyTextureEditor/Private/Tools/VectorDrawingTool/OdysseyTextureEditorVectorDrawingTool.cpp
@@ -0,0 +1,115 @@
+// IDDN FR.001.250001.005.S.P.2019.000.00000
+// ILIAD is subject to copyright laws and is the legal and intellectual property of Praxinos,Inc - Year of publishing 2022
+
+#include "Tools/VectorDrawingTool/OdysseyTextureEditorVectorDrawingTool.h"
+#include "LayerStack/OdysseyTextureLayerImageVector.h"
+
+#define LOCTEXT_NAMESPACE "OdysseyTextureEditorVectorDrawingTool"
+
+//--------------------------------------------------------------------------------------
+//----------------------------------------------------------- Construction / Destruction
+UOdysseyTextureEditorVectorDrawingTool::~UOdysseyTextureEditorVectorDrawingTool()
+{
+}
+
+UOdysseyTextureEditorVectorDrawingTool::UOdysseyTextureEditorVectorDrawingTool()
+{
+}
+
+//--------------------------------------------------------------------------------------
+//---------------------------------------------------------------- OdysseyPainterEditorTool overrides
+
+void
+UOdysseyTextureEditorVectorDrawingTool::Activate()
+{
+    UOdysseyTextureLayerStack::OnCurrentLayerChanged().AddUObject(this, &UOdysseyTextureEditorVectorDrawingTool::OnCurrentLayerChanged);
+    Load();
+    Super::Activate();
+}
+
+void
+UOdysseyTextureEditorVectorDrawingTool::Load()
+{
+	//Define the new active tool based on the layer type
+	class UOdysseyTextureLayerImageVector* layer = GetLayer();
+	if (!layer)
+		return;
+
+}
+
+void
+UOdysseyTextureEditorVectorDrawingTool::Inactivate()
+{
+	UOdysseyTextureLayerStack::OnCurrentLayerChanged().RemoveAll(this);
+    Super::Inactivate();
+
+}
+
+void
+UOdysseyTextureEditorVectorDrawingTool::Unload()
+{
+
+}
+
+bool
+UOdysseyTextureEditorVectorDrawingTool::IsActivable() const
+{
+	if (!Super::IsActivable())
+        return false; 
+
+	//Check for currentlayer
+	UOdysseyTextureLayerImageVector* layer = GetLayer();
+	if (!layer)
+		return false;
+
+	bool isActive = UOdysseyLayerFunctionLibrary::IsLayerActivatedInStack(layer);
+	bool isLocked = UOdysseyLayerFunctionLibrary::IsLayerLockedInStack(layer);
+    return isActive && !isLocked;
+}
+
+bool
+UOdysseyTextureEditorVectorDrawingTool::CanDraw()
+{	
+    if (!Super::CanDraw())
+        return false; 
+
+	UOdysseyTextureLayerImageVector* layer = GetLayer();
+	if (!layer)
+		return false;
+
+	bool isActive = UOdysseyLayerFunctionLibrary::IsLayerActivatedInStack(layer);
+	bool isLocked = UOdysseyLayerFunctionLibrary::IsLayerLockedInStack(layer);
+    return !isActive || isLocked;
+}
+
+void
+UOdysseyTextureEditorVectorDrawingTool::OnCurrentLayerChanged(UOdysseyLayerStack* iLayerStack)
+{
+	//ensure iLayerstack is the one the tool is working on
+	UOdysseyTextureLayerStack* layerstack = GetEditorAs<FOdysseyTextureEditor>()->LayerStack();
+	if ( !iLayerStack || !layerstack || layerstack != iLayerStack )
+		return;
+
+	//If not activable => Inactivate
+	if (!IsActivable())
+	{
+		Inactivate(); //close the tool
+		return;
+	}
+
+	//Reload the tool to edit the new layer
+	Unload();
+	Load();
+}
+
+UOdysseyTextureLayerImageVector*
+UOdysseyTextureEditorVectorDrawingTool::GetLayer() const
+{
+	UOdysseyTextureLayerStack* layerstack = GetEditorAs<FOdysseyTextureEditor>()->LayerStack();
+	if ( !layerstack )
+		return nullptr;
+
+	return Cast<UOdysseyTextureLayerImageVector>(layerstack->CurrentLayer.Get());
+}
+
+#undef LOCTEXT_NAMESPACE
