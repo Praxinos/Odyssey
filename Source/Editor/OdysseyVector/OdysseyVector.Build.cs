c1562433bd85a11c73121f92b59b181307e0498b
Gary-Praxinos <gary.gabriel@praxinos.coop>
2023-01-26 13:46:49 +0100

    fix: add OdysseyVector.Build.cs

---
+++ Source/Editor/OdysseyVector/OdysseyVector.Build.cs
@@ -0,0 +1,51 @@
+// IDDN.FR.001.250001.006.S.P.2019.000.00000
+// ILIAD is subject to copyright laws and is the legal and intellectual property of Praxinos,Inc - Year of publishing 2022
+
+using System.IO;
+using UnrealBuildTool;
+
+public class OdysseyVector : ModuleRules
+{
+    public OdysseyVector(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[] {
+                "Core",
+                "CoreUObject",
+                "InputCore",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "OdysseyCore",
+                "OdysseyStyle",
+                "blend2d",
+                "ULIS",
+                "ULISLoader"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[] {
+                "Core",
+                "CoreUObject",
+                "InputCore",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "OdysseyCore",
+                "OdysseyStyle",
+                "blend2d",
+                "ULIS",
+                "ULISLoader"
+            }
+        );
+
+        PublicIncludePaths.AddRange(
+            new string[] {
+                Path.Combine(ModuleDirectory, "Public"),
+            }
+        );
+    }
+}
