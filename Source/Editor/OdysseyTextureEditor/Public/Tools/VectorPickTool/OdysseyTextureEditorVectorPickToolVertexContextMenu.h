2242cbd8bced72b642f78b62a6b753d9bde9ef14
Gary-Praxinos <gary.gabriel@praxinos.coop>
2023-07-03 12:55:42 +0200

    fix: PickTool HUD in vertex mode - adding missing files

---
+++ Source/Editor/OdysseyTextureEditor/Public/Tools/VectorPickTool/OdysseyTextureEditorVectorPickToolVertexContextMenu.h
@@ -0,0 +1,24 @@
+// IDDN.FR.001.250001.006.S.P.2019.000.00000
+// ILIAD is subject to copyright laws and is the legal and intellectual property of Praxinos,Inc - Year of publishing 2022
+
+#pragma once
+
+#include "Tools/VectorPickTool/OdysseyPainterEditorVectorPickToolVertexContextMenu.h"
+
+class FOdysseyTextureEditor;
+
+class ODYSSEYTEXTUREEDITOR_API FOdysseyTextureEditorVectorPickToolVertexContextMenu :
+	public FOdysseyPainterEditorVectorPickToolVertexContextMenu
+{
+    public:
+        // Construction / Destruction
+        virtual ~FOdysseyTextureEditorVectorPickToolVertexContextMenu();
+        FOdysseyTextureEditorVectorPickToolVertexContextMenu( FOdysseyTextureEditor* iEditor );
+
+    protected:
+        // FOdysseyTextureEditorVectorPickToolVertexContextMenu interface
+        virtual void BindShortcuts(FBaseToolkit* iToolkit) override;
+        //virtual void ExtendMenu( FToolMenuOwner iOwner, FName iMenuName ) override;
+        //virtual void OnToolkitInitialized( FBaseToolkit* iToolkit ) override;
+};
+
