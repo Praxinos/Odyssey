424668fa02d6c6372ea7ba1fac2d310bca8921a3
Eric2-Praxinos <44882044+Eric2-Praxinos@users.noreply.github.com>
2023-07-21 16:11:37 +0200

    feat : Add Blueprint Library to select editor Mode

---
+++ Source/Editor/OdysseyViewportDrawingEditor/Public/OdysseyViewportDrawingEditorBlueprintLibrary.h
@@ -0,0 +1,65 @@
+// Copyright Praxinos, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OdysseyViewportDrawingEditorBlueprintLibrary.generated.h"
+
+UCLASS()
+class UOdysseyViewportDrawingEditorBlueprintLibrary :
+    public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+    /** Starts an analytics session without any custom attributes specified */
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static void ActivateViewportEditorMode(const FName& EditorMode);
+    
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static FName GetIliadViewportDrawingEditorMode();
+
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static FName GetModelingEditorMode();
+
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static FName GetNoneEditorMode();
+
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static FName GetDefaultEditorMode();
+
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static FName GetPlacementEditorMode();
+
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static FName GetMeshPaintEditorMode();
+
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static FName GetLandscapeEditorMode();
+
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static FName GetFoliageEditorMode();
+
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static FName GetLevelEditorMode();
+
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static FName GetStreamingLevelEditorMode();
+
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static FName GetPhysicsEditorMode();
+
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static FName GetActorPickerEditorMode();
+
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static FName GetSceneDepthPickerEditorMode();
+
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static FName GetAnimationEditorMode();
+
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static FName GetBrushEditingEditorMode();
+
+    UFUNCTION(BlueprintCallable, Category="Odyssey|Editor")
+    static FName GetFractureEditorMode();
+};
