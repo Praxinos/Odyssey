0363fd4e43c77d6c0f4f0dcf3f0bb9f2567e15c3
mike-praxinos <54707550+mike-praxinos@users.noreply.github.com>
2023-10-18 10:08:24 +0200

    feat: add a virtual environment feat: update .gitignore

--- build-package.py
+++ build-package.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # IDDN.FR.001.220036.001.S.P.2021.000.00000
 # EPOS is subject to copyright © laws and is the legal and intellectual property of Praxinos,Inc - Year of publishing 2022
 
