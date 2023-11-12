9c48d177ae121f95bc15937ca3996622a4564b8a
Gary-Praxinos <gary.gabriel@praxinos.coop>
2023-11-12 22:40:29 +0100

    feat: tool menu in Edit Menu for the Animation Editor

--- Source/Editor/OdysseyTextureEditor/Private/OdysseyTextureEditorModule.cpp
+++ Source/Editor/OdysseyTextureEditor/Private/OdysseyTextureEditorModule.cpp
@@ -42,17 +42,21 @@ FOdysseyTextureEditorModule::CreateOdysseyTextureEditor( UTexture2D* iTexture )
 
     TSharedPtr<FOdysseyTextureEditorToolkit> toolkit = MakeShared<FOdysseyTextureEditorToolkit>();
     toolkit->Initialize(iTexture, editor);
-	TSharedPtr<FExtender> menuExtender = MakeShareable(new FExtender());
 
+    //----- Extend the Edit menu using the FMenuBuilder API
+	TSharedPtr<FExtender> menuExtender = MakeShareable(new FExtender());
+    // Extend the Edit menu after the "Configuration" category
 	menuExtender->AddMenuExtension(
 		"Configuration",
 		EExtensionHook::After,
 		NULL,
 		FMenuExtensionDelegate::CreateRaw( editor.Get(), &FOdysseyPainterEditor::AddEditMenuEntry )
 	);
-
 	toolkit->AddMenuExtender( menuExtender );
+    // Rebuild the the menu bar and take into account the above extender. 
+    // FOdysseyPainterEditor::AddEditMenuEntry() will then be called each time the Edit menu needs to be shown.
 	toolkit->RegenerateMenusAndToolbars();
+    //-----
 
 	TSharedPtr<FOdysseyTextureEditorSource> source = MakeShared<FOdysseyTextureEditorSource>(iTexture);
 	editor->SetSource(source);
