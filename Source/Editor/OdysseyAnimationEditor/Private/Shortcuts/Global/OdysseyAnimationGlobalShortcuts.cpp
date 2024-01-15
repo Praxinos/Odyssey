978745c732ac33be8ce97e105d79fce667f346c0
Eric2-Praxinos <44882044+Eric2-Praxinos@users.noreply.github.com>
2024-01-15 13:36:57 +0100

    Fix : wrong include

--- Source/Editor/OdysseyAnimationEditor/Private/Shortcuts/Global/OdysseyAnimationGlobalShortcuts.cpp
+++ Source/Editor/OdysseyAnimationEditor/Private/Shortcuts/Global/OdysseyAnimationGlobalShortcuts.cpp
@@ -1,7 +1,7 @@
 // IDDN FR.001.250001.005.S.P.2019.000.00000
 // ILIAD is subject to copyright laws and is the legal and intellectual property of Praxinos,Inc - Year of publishing 2022
 
-#include "Shortcuts/Global/OdysseyAnimationGlobalCellsShortcuts.h"
+#include "Shortcuts/Global/OdysseyAnimationGlobalShortcuts.h"
 #include "Shortcuts/Global/OdysseyAnimationGlobalTimelineShortcuts.h"
 
 #define LOCTEXT_NAMESPACE "AnimationEditor"
