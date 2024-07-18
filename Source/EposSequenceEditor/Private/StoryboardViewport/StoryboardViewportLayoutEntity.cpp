c9ed1d8f64514ff965a7dbe270fda0a62063de29
Thomas <thomas.schmitt@praxinos.coop>
2024-07-18 14:43:16 +0200

    fix: compilation on MacOS

--- Source/EposSequenceEditor/Private/StoryboardViewport/StoryboardViewportLayoutEntity.cpp
+++ Source/EposSequenceEditor/Private/StoryboardViewport/StoryboardViewportLayoutEntity.cpp
@@ -1,8 +1,6 @@
 // IDDN.FR.001.060015.008.S.X.2019.000.00000
 // EPOS is subject to copyright © laws and is the legal and intellectual property of Praxinos,Inc - Year of publishing 2022
 
-#pragma once
-
 #include "StoryboardViewport/StoryboardViewportLayoutEntity.h"
 
 #include "Widgets/SWidget.h"
