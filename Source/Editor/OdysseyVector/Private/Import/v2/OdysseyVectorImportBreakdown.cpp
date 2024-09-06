d9da25d7fcdbe790bcf3b8ef9d20bdd2865f7a78
Gary-Praxinos <gary.gabriel@praxinos.coop>
2024-09-06 19:37:43 +0200

    missing source files

---
+++ Source/Editor/OdysseyVector/Private/Import/v2/OdysseyVectorImportBreakdown.cpp
@@ -0,0 +1,105 @@
+#include "Import/v2/OdysseyVectorImport.h"
+#include "Palette/OdysseyPalette.h"
+#include "Engine/ObjectLibrary.h"
+#include "AssetRegistry/AssetRegistryModule.h"
+
+// from module OdysseyFile
+#include "OdysseyFile.h"
+#include "InbetweenerTag/InbetweenerPoint.h"
+#include "InbetweenerTag/InbetweenerGrid.h"
+#include "InbetweenerTag/InbetweenerBreakdown.h"
+#include "OdysseyVectorTagInbetweener.h"
+
+void
+FOdysseyVectorImportV2::ReadBreakdown( FInbetweenerBreakdown& iBreakdown
+                                     , uint64 iChunkEnd
+                                     , FArchive &Ar )
+{
+    FOdysseyFile::ReadChunks( iChunkEnd
+                            , Ar
+                            , [ this
+                              , &iBreakdown ]( uint32 iChunkID
+                                             , uint64 iChunkLen
+                                             , FArchive &Ar ) -> void
+        {
+            switch( iChunkID )
+            {
+                case FOdysseyFile::VectorV2::CHUNK_BREAKDOWN_TRANSFORM:  // container
+                break;
+
+                case FOdysseyFile::VectorV2::CHUNK_BREAKDOWN_TRANSFORM_TRANSLATION:
+                    double translationX;
+                    double translationY;
+
+                    Ar << translationX;
+                    Ar << translationY;
+
+                    iBreakdown.Translate( translationX, translationY );
+                    //iBreakdown.UpdateMatrix();
+                break;
+
+                case FOdysseyFile::VectorV2::CHUNK_BREAKDOWN_TRANSFORM_ROTATION:
+                    double rotation;
+
+                    Ar << rotation;
+
+                    iBreakdown.Rotate( rotation );
+                    //iBreakdown.UpdateMatrix();
+                break;
+
+                case FOdysseyFile::VectorV2::CHUNK_BREAKDOWN_TRANSFORM_SCALING:
+                    double scalingX;
+                    double scalingY;
+
+                    Ar << scalingX;
+                    Ar << scalingY;
+
+                    iBreakdown.Scale( scalingX, scalingY );
+                    //iBreakdown.UpdateMatrix();
+                break;
+
+                case FOdysseyFile::VectorV2::CHUNK_BREAKDOWN_GRIDGEOMETRY:
+                {
+                    uint32 numQuadX = iBreakdown.GetInbetweenerTag()->GetGridNumQuadX();
+                    uint32 numQuadY = iBreakdown.GetInbetweenerTag()->GetGridNumQuadY();
+
+                    std::vector<FInbetweenerPoint>& gridPointbuffer = iBreakdown.GetGrid()->GetPointBuffer();
+                    std::vector<::ULIS::FVec2D> sourcePosition;
+                    std::vector<::ULIS::FVec2D> targetPosition;
+
+                    if( numQuadX && numQuadY )
+                    {
+                        uint32 pointCount = ( numQuadX + 1 ) * ( numQuadY + 1 );
+
+                        sourcePosition.reserve( pointCount );
+                        targetPosition.reserve( pointCount );
+
+                        for( uint32 i = 0; i < pointCount; i++ )
+                        {
+                            double sourceX;
+                            double sourceY;
+                            double targetX;
+                            double targetY;
+
+                            Ar << sourceX;
+                            Ar << sourceY;
+                            Ar << targetX;
+                            Ar << targetY;
+
+                            sourcePosition.emplace_back( sourceX, sourceY );
+                            targetPosition.emplace_back( targetX, targetY );
+                        }
+                    }
+
+                    iBreakdown.GetGrid()->SetGeometry( sourcePosition, eInbetweenerPointPositionType::SourcePosition );
+                    iBreakdown.GetGrid()->SetGeometry( targetPosition, eInbetweenerPointPositionType::TargetPosition );
+                }
+                break;
+
+                default:
+                // Mandatory
+                    Ar.Seek( Ar.Tell() + iChunkLen );
+                break;
+            }    
+        } );
+}
