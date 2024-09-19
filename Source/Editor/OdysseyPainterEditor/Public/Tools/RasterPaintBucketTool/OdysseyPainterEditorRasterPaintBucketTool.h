0389cd3266b818444d08d7b80dd1d6cdcf3da05e
Eric2-Praxinos <44882044+Eric2-Praxinos@users.noreply.github.com>
2024-09-19 11:36:35 +0200

    feat: define default values for Raster Paint Bucket Tool

--- Source/Editor/OdysseyPainterEditor/Public/Tools/RasterPaintBucketTool/OdysseyPainterEditorRasterPaintBucketTool.h
+++ Source/Editor/OdysseyPainterEditor/Public/Tools/RasterPaintBucketTool/OdysseyPainterEditorRasterPaintBucketTool.h
@@ -84,15 +84,15 @@ private:
 
 public:
     UPROPERTY( EditAnywhere, Category=RasterPaintBucketTool)
-    EOdysseyRasterPaintBucketToolSource Source = EOdysseyRasterPaintBucketToolSource::CurrentLayer;
+    EOdysseyRasterPaintBucketToolSource Source = EOdysseyRasterPaintBucketToolSource::AllLayers;
     UPROPERTY( EditAnywhere, Category=RasterPaintBucketTool)
-    EOdysseyRasterPaintBucketToolColorToleranceSource ColorToleranceSource;
+    EOdysseyRasterPaintBucketToolColorToleranceSource ColorToleranceSource = EOdysseyRasterPaintBucketToolColorToleranceSource::ColorAndTransparency;
     UPROPERTY( EditAnywhere, Category=RasterPaintBucketTool, meta = ( ClampMin = "0", ClampMax = "100", UIMin = "0", UIMax = "100", Delta = "1", Multiple="1", Units="Percent") )
-    float ColorTolerance; //0 - 100%
+    float ColorTolerance = 5; //0 - 100%
     UPROPERTY( EditAnywhere, Category=RasterPaintBucketTool, meta=(LinearDeltaSensitivity=1) )
-    float Expansion; //pixels positive and negative
+    float Expansion = 2; //pixels positive and negative
     UPROPERTY( EditAnywhere, Category=RasterPaintBucketTool, meta=(ClampMin=0, UIMin=0, LinearDeltaSensitivity=1) )
-    float GapTolerance; //pixels positive only
+    float GapTolerance = 2; //pixels positive only
     UPROPERTY( EditAnywhere, Category=RasterPaintBucketTool)
     TArray<FLinearColor> IncludeColors;
     
