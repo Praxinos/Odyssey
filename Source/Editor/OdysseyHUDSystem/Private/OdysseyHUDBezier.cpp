60aba03f98c0b7e4997f9fe57b0f5a3a6b73e72a
Thomas-Praxinos <thomas.schmitt@praxinos.coop>
2024-10-30 10:55:51 +0100

    fix: change order of some includes

--- Source/Editor/OdysseyHUDSystem/Private/OdysseyHUDBezier.cpp
+++ Source/Editor/OdysseyHUDSystem/Private/OdysseyHUDBezier.cpp
@@ -1,9 +1,10 @@
 // IDDN.FR.001.250001.006.S.P.2019.000.00000
 // ILIAD is subject to copyright laws and is the legal and intellectual property of Praxinos,Inc - Year of publishing 2022
 
+#include "OdysseyHUDBezier.h"
+
 #include "BatchedElements.h"
 #include "CanvasTypes.h"
-#include "OdysseyHUDBezier.h"
 
 #include "ULISLoaderModule.h"
 
--- Source/Editor/OdysseyHUDSystem/Private/OdysseyHUDHandle.cpp
+++ Source/Editor/OdysseyHUDSystem/Private/OdysseyHUDHandle.cpp
@@ -1,10 +1,11 @@
 // IDDN.FR.001.250001.006.S.P.2019.000.00000
 // ILIAD is subject to copyright laws and is the legal and intellectual property of Praxinos,Inc - Year of publishing 2022
 
+#include "OdysseyHUDHandle.h"
+
 #include "CanvasTypes.h"
 #include "Engine/Texture.h"
 #include "Input/OdysseyPoint.h"
-#include "OdysseyHUDHandle.h"
 #include "OdysseyHUDElement.h"
 #include "TextureResource.h"
 
