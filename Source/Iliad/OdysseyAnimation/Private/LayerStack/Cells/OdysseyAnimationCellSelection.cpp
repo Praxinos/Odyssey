858879f12644ace5fbad4ff8b81678123caf9c75
Thomas-Praxinos <thomas.schmitt@praxinos.coop>
2024-11-19 16:32:57 +0100

    fix: missing include in animationCellSelection

--- Source/Iliad/OdysseyAnimation/Private/LayerStack/Cells/OdysseyAnimationCellSelection.cpp
+++ Source/Iliad/OdysseyAnimation/Private/LayerStack/Cells/OdysseyAnimationCellSelection.cpp
@@ -1,6 +1,8 @@
 // IDDN.FR.001.250001.006.S.P.2019.000.00000
 // ILIAD is subject to copyright laws and is the legal and intellectual property of Praxinos,Inc - Year of publishing 2023#include "LayerStack/Cells/OdysseyAnimationCellSelection.h"
 
+#include "LayerStack/Cells/OdysseyAnimationCellSelection.h"
+
 #include "OdysseyAnimation.h"
 #include "OdysseyLayerStack.h"
 #include "LayerStack/Layers/OdysseyAnimationLayer.h"
