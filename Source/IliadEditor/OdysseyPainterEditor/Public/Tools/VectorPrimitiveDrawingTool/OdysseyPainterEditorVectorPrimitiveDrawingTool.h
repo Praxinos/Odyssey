d8cc77c91a62cf22cfc4e5c0d1f9fa46e43929bf
Eric2-Praxinos <44882044+Eric2-Praxinos@users.noreply.github.com>
2025-01-21 13:53:42 +0100

    feat: Add vector primitive drawing tool toolbar widgets

--- Source/IliadEditor/OdysseyPainterEditor/Public/Tools/VectorPrimitiveDrawingTool/OdysseyPainterEditorVectorPrimitiveDrawingTool.h
+++ Source/IliadEditor/OdysseyPainterEditor/Public/Tools/VectorPrimitiveDrawingTool/OdysseyPainterEditorVectorPrimitiveDrawingTool.h
@@ -43,6 +43,8 @@ class ODYSSEYPAINTEREDITOR_API UOdysseyPainterEditorVectorPrimitiveDrawingTool :
 
         virtual FText GetTooltip() const override;
 
+        virtual void ExtendToolbar( FToolBarBuilder& iBuilder ) override;
+
     protected:
         //OdysseyPainterVectorBaseEditorTool overrides
         virtual uint64 LoadVector( FOdysseyVectorGroupPaint* iScene ) override;
