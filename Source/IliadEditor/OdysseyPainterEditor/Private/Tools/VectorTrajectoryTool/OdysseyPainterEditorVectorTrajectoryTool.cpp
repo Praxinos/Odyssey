03c879fe4a4a3e422dfb1f31b9c6dd48182a58ae
Eric2-Praxinos <44882044+Eric2-Praxinos@users.noreply.github.com>
2025-01-21 14:07:53 +0100

    feat: Add vector trajectory tool toolbar widgets

--- Source/IliadEditor/OdysseyPainterEditor/Private/Tools/VectorTrajectoryTool/OdysseyPainterEditorVectorTrajectoryTool.cpp
+++ Source/IliadEditor/OdysseyPainterEditor/Private/Tools/VectorTrajectoryTool/OdysseyPainterEditorVectorTrajectoryTool.cpp
@@ -8,6 +8,7 @@
 #include "ISinglePropertyView.h"
 #include "PainterEditor/OdysseyPainterEditorSource.h"
 #include "Misc/MessageDialog.h"
+#include "SOdysseySinglePropertyView.h"
 // Vector engine
 #include "OdysseyVectorGroupPaint.h"
 #include "OdysseyVectorSharedEnv.h"
@@ -661,6 +662,35 @@ UOdysseyPainterEditorVectorTrajectoryTool::DeleteRoute()
 
     // redraw
     scene->GetEngine()->Invalidate( 0 );
+}
+
+void
+UOdysseyPainterEditorVectorTrajectoryTool::ExtendToolbar( FToolBarBuilder& iBuilder )
+{
+    Super::ExtendToolbar(iBuilder);
+
+    iBuilder.BeginSection( NAME_None );
+
+    iBuilder.AddWidget(
+        SNew(SBox)
+        .Padding(10.f, 0.f, 10.f, 0.f)
+        [
+            SNew(SOdysseySinglePropertyView, this, GET_MEMBER_NAME_CHECKED( UOdysseyPainterEditorVectorTrajectoryTool, ShowInbetweens ), FSinglePropertyParams())
+            .InnerPadding(10.f)
+        ]
+    );
+
+    iBuilder.AddWidget(
+        SNew(SBox)
+        .Padding(10.f, 0.f, 10.f, 0.f)
+        [
+            SNew(SOdysseySinglePropertyView, this, GET_MEMBER_NAME_CHECKED( UOdysseyPainterEditorVectorTrajectoryTool, EditionMode ), FSinglePropertyParams())
+            .InnerPadding(10.f)
+            .ValueWidthOverride(100.f)
+        ]
+    );
+
+    iBuilder.EndSection();
 }
 
 /* TSharedRef<SWidget>
--- Source/IliadEditor/OdysseyPainterEditor/Public/Tools/VectorTrajectoryTool/OdysseyPainterEditorVectorTrajectoryTool.h
+++ Source/IliadEditor/OdysseyPainterEditor/Public/Tools/VectorTrajectoryTool/OdysseyPainterEditorVectorTrajectoryTool.h
@@ -52,6 +52,8 @@ class ODYSSEYPAINTEREDITOR_API UOdysseyPainterEditorVectorTrajectoryTool : publi
 
         virtual FText GetTooltip() const override;
 
+        virtual void ExtendToolbar( FToolBarBuilder& iBuilder ) override;
+
     protected:
         //OdysseyPainterVectorBaseEditorTool overrides
         virtual uint64 LoadVector( FOdysseyVectorGroupPaint* iScene ) override;
