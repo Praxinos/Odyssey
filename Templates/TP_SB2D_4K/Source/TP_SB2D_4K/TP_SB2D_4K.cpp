2e5535602e7042b104cb02da97c55a4de85c8366
Fabrice Debarge <fabrice.debarge@praxinos.coop>
2025-03-06 23:31:03 +0100

    chore: rename some files and content to match the template naming convention related to UltraHD 4K

--- Templates/TP_SB2D_4K/Source/TP_SB2D_4K/TP_SB2D_4K.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TP_SB2D_4K.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TP_SB2D_4K, "TP_SB2D_4K" );
