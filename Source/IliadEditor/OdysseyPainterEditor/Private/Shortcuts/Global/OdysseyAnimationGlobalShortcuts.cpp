8210ff4cfdc06082f95ef1fd2921fe9445f85461
mike-praxinos <54707550+mike-praxinos@users.noreply.github.com>
2025-03-14 15:45:59 +0100

    fix: sort include order

--- Source/IliadEditor/OdysseyPainterEditor/Private/Shortcuts/Global/OdysseyAnimationGlobalShortcuts.cpp
+++ Source/IliadEditor/OdysseyPainterEditor/Private/Shortcuts/Global/OdysseyAnimationGlobalShortcuts.cpp
@@ -1,8 +1,9 @@
 // IDDN.FR.001.060015.013.S.X.2019.000.00000
 // ODYSSEY is subject to copyright laws and is the legal and intellectual property of Praxinos,Inc - Year of publishing 2022
 
-#include "OdysseyPainterEditor.h"
 #include "Shortcuts/Global/OdysseyAnimationGlobalShortcuts.h"
+
+#include "OdysseyPainterEditor.h"
 #include "Shortcuts/Global/OdysseyAnimationGlobalCellsShortcuts.h"
 #include "Shortcuts/Global/OdysseyAnimationGlobalTimelineShortcuts.h"
 #include "Shortcuts/Global/OdysseyAnimationGlobalTimelineToolsShortcuts.h"
