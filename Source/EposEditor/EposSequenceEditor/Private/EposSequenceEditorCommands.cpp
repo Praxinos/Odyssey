8f45e4d42c9f7d2431a1c3d7792fade1a94559b0
mike-praxinos <54707550+mike-praxinos@users.noreply.github.com>
2025-03-28 14:20:38 +0100

    fix: update Storyboard viewport name to Odyssey viewport

--- Source/EposEditor/EposSequenceEditor/Private/EposSequenceEditorCommands.cpp
+++ Source/EposEditor/EposSequenceEditor/Private/EposSequenceEditorCommands.cpp
@@ -30,19 +30,19 @@ FEposSequenceEditorCommands::RegisterCommands()
 {
     UI_COMMAND( NewStoryboardWithSettings,          "Start Storyboarding", "Create a new storyboard with settings", EUserInterfaceActionType::Button, FInputChord() );
     UI_COMMAND( NewStoryboardImportImageSequence,   "Import Image Sequence", "Create a new storyboard from a sequence of images", EUserInterfaceActionType::Button, FInputChord() );
-    UI_COMMAND( ToggleStoryboardViewportCommand,    "Storyboard Viewport", "A viewport layout tailored to storyboard preview", EUserInterfaceActionType::RadioButton, FInputChord() );
+    UI_COMMAND( ToggleStoryboardViewportCommand,    "Odyssey Viewport", "A viewport layout tailored to storyboard/animation preview", EUserInterfaceActionType::RadioButton, FInputChord() );
 
     //---
 
-    UI_COMMAND( StoryboardViewportAdd10Rotate,      "Storyboard Viewport Rotation +10°", "+10° to the viewport rotation", EUserInterfaceActionType::Button, FInputChord() );
-    UI_COMMAND( StoryboardViewportSubstract10Rotate,"Storyboard Viewport Rotation -10°", "-10° to the viewport rotation", EUserInterfaceActionType::Button, FInputChord() );
+    UI_COMMAND( StoryboardViewportAdd10Rotate,      "Odyssey Viewport Rotation +10°", "+10° to the viewport rotation", EUserInterfaceActionType::Button, FInputChord() );
+    UI_COMMAND( StoryboardViewportSubstract10Rotate,"Odyssey Viewport Rotation -10°", "-10° to the viewport rotation", EUserInterfaceActionType::Button, FInputChord() );
 
-    UI_COMMAND( StoryboardViewportAdd10Zoom,      "Storyboard Viewport Zoom +10%", "+10% to the viewport Zoom", EUserInterfaceActionType::Button, FInputChord() );
-    UI_COMMAND( StoryboardViewportSubstract10Zoom,"Storyboard Viewport Zoom -10%", "-10% to the viewport Zoom", EUserInterfaceActionType::Button, FInputChord() );
-    UI_COMMAND( StoryboardViewportResetPanZoomRotate, "Storyboard Viewport Reset Pan, Zoom and Rotation ", "Resets the viewport Pan, Zoom and Rotation values", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control | EModifierKey::Shift, EKeys::M) );
+    UI_COMMAND( StoryboardViewportAdd10Zoom,      "Odyssey Viewport Zoom +10%", "+10% to the viewport Zoom", EUserInterfaceActionType::Button, FInputChord() );
+    UI_COMMAND( StoryboardViewportSubstract10Zoom,"Odyssey Viewport Zoom -10%", "-10% to the viewport Zoom", EUserInterfaceActionType::Button, FInputChord() );
+    UI_COMMAND( StoryboardViewportResetPanZoomRotate, "Odyssey Viewport Reset Pan, Zoom and Rotation ", "Resets the viewport Pan, Zoom and Rotation values", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control | EModifierKey::Shift, EKeys::M) );
 
-    FTextFormat rotation_label_format( LOCTEXT( "storyboard-viewport-rotation-label", "Set Storyboard Viewport to {0}°" ) );
-    FTextFormat rotation_tooltip_format( LOCTEXT( "storyboard-viewport-rotation-tooltip", "Set the viewport rotation to {0}°" ) );
+    FTextFormat rotation_label_format( LOCTEXT( "odyssey-viewport-rotation-label", "Set Odyssey Viewport to {0}°" ) );
+    FTextFormat rotation_tooltip_format( LOCTEXT( "odyssey-viewport-rotation-tooltip", "Set the viewport rotation to {0}°" ) );
 
     TArray<int32> angles = { -135, -90, -45, 0, 45, 90, 135, 180 };
     for( auto angle : angles )
@@ -50,7 +50,7 @@ FEposSequenceEditorCommands::RegisterCommands()
         StoryboardViewportSetRotationX.Add( angle,
                                             FUICommandInfoDecl(
                                                 this->AsShared(),
-                                                FName( *FString::Printf( TEXT( "StoryboardViewportRotate-%d" ), angle ) ),
+                                                FName( *FString::Printf( TEXT( "OdysseyViewportRotate-%d" ), angle ) ),
                                                 FText::Format( rotation_label_format, angle ),
                                                 FText::Format( rotation_tooltip_format, angle ),
                                                 ViewportRotationBundleName )
@@ -59,8 +59,8 @@ FEposSequenceEditorCommands::RegisterCommands()
         );
     }
 
-    FTextFormat zoom_label_format( LOCTEXT( "storyboard-viewport-zoom-label", "Set Storyboard Viewport to {0}%" ) );
-    FTextFormat zoom_tooltip_format( LOCTEXT( "storyboard-viewport-zoom-tooltip", "Set the viewport zoom to {0}%" ) );
+    FTextFormat zoom_label_format( LOCTEXT( "odyssey-viewport-zoom-label", "Set Odyssey Viewport to {0}%" ) );
+    FTextFormat zoom_tooltip_format( LOCTEXT( "odyssey-viewport-zoom-tooltip", "Set the viewport zoom to {0}%" ) );
 
     TArray<float> zooms = { 0.25f, 0.5f, 1.f, 2.f, 4.f };
     for( auto zoom : zooms )
@@ -68,7 +68,7 @@ FEposSequenceEditorCommands::RegisterCommands()
         StoryboardViewportSetZoomX.Add( zoom,
                                             FUICommandInfoDecl(
                                                 this->AsShared(),
-                                                FName( *FString::Printf( TEXT( "StoryboardViewportRotate-%d" ), FMath::RoundToInt32(zoom * 100.f) ) ),
+                                                FName( *FString::Printf( TEXT( "OdysseyViewportRotate-%d" ), FMath::RoundToInt32(zoom * 100.f) ) ),
                                                 FText::Format( zoom_label_format, FMath::RoundToInt32(zoom * 100.f) ),
                                                 FText::Format( zoom_tooltip_format, FMath::RoundToInt32(zoom * 100.f) ),
                                                 ViewportZoomBundleName )
@@ -77,8 +77,8 @@ FEposSequenceEditorCommands::RegisterCommands()
         );
     }
 
-    UI_COMMAND( StoryboardViewportHoldToPanZoom, "Storyboard Viewport Pan/Zoom (Hold)", "Hold this key to pan (left mouse button) or zoom (right mouse button) the viewport", EUserInterfaceActionType::Button, FInputChord(EKeys::M) );
-    UI_COMMAND( StoryboardViewportHoldToRotate, "Storyboard Viewport Rotate (Hold)", "Hold this key to to rotate the viewport using the left mouse button", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control, EKeys::M) );
+    UI_COMMAND( StoryboardViewportHoldToPanZoom, "Odyssey Viewport Pan/Zoom (Hold)", "Hold this key to pan (left mouse button) or zoom (right mouse button) the viewport", EUserInterfaceActionType::Button, FInputChord(EKeys::M) );
+    UI_COMMAND( StoryboardViewportHoldToRotate, "Odyssey Viewport Rotate (Hold)", "Hold this key to to rotate the viewport using the left mouse button", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control, EKeys::M) );
 
     //---
 
