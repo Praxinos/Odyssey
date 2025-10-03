b74b59c7a872c8524de39b15ca7010fbb28eddcf
mike-praxinos <54707550+mike-praxinos@users.noreply.github.com>
2025-10-03 15:52:56 +0200

    Revert "fix: Crash when copy pasting animation track in level sequence"
    This reverts commit 9186e502ef76fea4a3621183114256b0ca0b9e2a.

--- Source/Iliad/OdysseyAnimationTracks/Private/OdysseyAnimationTimelineSection.cpp
+++ Source/Iliad/OdysseyAnimationTracks/Private/OdysseyAnimationTimelineSection.cpp
@@ -250,9 +250,6 @@ FFrameNumber
 UOdysseyAnimationTimelineSection::ConvertFrameFromTimelineToSequence( FFrameNumber iFrameInTimeline )
 {
     UMovieSceneSequence* sequence = GetTypedOuter<UMovieSceneSequence>();
-    if (!sequence)
-        return FFrameNumber(0);
-
     // Default value if animation doesn't exist
     // Otherwise animation validity must be checked everywhere outside this function
     FFrameRate animation_framerate( 24000, 1000 );
