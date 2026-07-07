2f033f5497fff59ca2896137f2a051150acae62c
Thomas-Praxinos <thomas.schmitt@praxinos.coop>
2026-07-07 17:05:50 +0200

    refactor: remove internal fixes for stylus input on mac

--- Source/IliadEditor/OdysseyPainterEditor/Private/OdysseyPainterEditorViewportClient.cpp
+++ Source/IliadEditor/OdysseyPainterEditor/Private/OdysseyPainterEditorViewportClient.cpp
@@ -1036,44 +1036,9 @@ void FOdysseyPainterEditorViewportClient::OnPacket(const UE::StylusInput::FStylu
         mPacketQueue.Enqueue(packetCopyWin);
         return;
     }
-#endif
 // FIX: MOVE WINTAB COORDINATES WHEN MAIN SCREEN IS NOT ON THE (TOP) LEFT OF USER PHYSICAL DESKTOP - AWAITING FOR EPIC PULL REQUEST VALIDATION
 
-// FIX: HAVE TO MANUALLY HANDLE UP AND DOWN UNTIL EPIC ACCEPT INTERNAL PULL REQUEST
-#if PLATFORM_MAC
-    UE::StylusInput::FStylusInputPacket packetCopyMac = iPacket;
-
-    if (iPacket.NormalPressure == 0)
-    {
-        mCurrentPenStatus = mCurrentPenStatus & ~UE::StylusInput::EPenStatus::CursorIsTouching;
-        if (mCurrentPacketType == UE::StylusInput::EPacketType::OnDigitizer)
-            mCurrentPacketType = UE::StylusInput::EPacketType::StylusUp;
-        else
-            mCurrentPacketType = UE::StylusInput::EPacketType::AboveDigitizer;
-    }
-
-    if (iPacket.NormalPressure != 0)
-    {
-        mCurrentPenStatus = mCurrentPenStatus | UE::StylusInput::EPenStatus::CursorIsTouching;
-        if (mCurrentPacketType != UE::StylusInput::EPacketType::OnDigitizer && mCurrentPacketType != UE::StylusInput::EPacketType::StylusDown)
-            mCurrentPacketType = UE::StylusInput::EPacketType::StylusDown;
-        else
-            mCurrentPacketType = UE::StylusInput::EPacketType::OnDigitizer;
-    }
-    else
-    {
-        mCurrentPenStatus = mCurrentPenStatus & ~UE::StylusInput::EPenStatus::CursorIsTouching;
-    }
-
-    packetCopyMac.PenStatus = mCurrentPenStatus;
-    packetCopyMac.Type = mCurrentPacketType;
-
-    if (packetCopyMac.Type == UE::StylusInput::EPacketType::StylusDown && mEventsConsumedSinceLastUp == 0)
-        ClearQueue();
-
-    mPacketQueue.Enqueue(packetCopyMac);
-    return;
-#else// FIX: HAVE TO MANUALLY HANDLE UP AND DOWN UNTIL EPIC ACCEPT INTERNAL PULL REQUEST
+#else
     if (iPacket.Type == UE::StylusInput::EPacketType::StylusDown && mEventsConsumedSinceLastUp == 0)
         ClearQueue();
 
