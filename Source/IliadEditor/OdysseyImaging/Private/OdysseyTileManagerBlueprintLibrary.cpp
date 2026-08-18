aac537d74a25643b79b421f3a9eb4fa1bfdd4e92
Eric Scholl <eric.scholl@praxinos.coop>
2026-08-18 23:12:01 +0200

    wip: add stats

---
+++ Source/IliadEditor/OdysseyImaging/Private/OdysseyTileManagerBlueprintLibrary.cpp
@@ -0,0 +1,42 @@
+// IDDN.FR.001.060015.015.S.X.2019.000.00000
+// ODYSSEY is subject to copyright © laws and is the legal and intellectual property of Praxinos,Inc - Year of publishing 2019
+
+#include "OdysseyTileManagerBlueprintLibrary.h"
+
+#include "OdysseyTileManager.h"
+
+int64
+UOdysseyTileManagerBlueprintLibrary::GetTileManagerNumTilesUncompressed()
+{
+    return FOdysseyTileManager::Get().GetNumTilesUncompressed();
+}
+
+int64
+UOdysseyTileManagerBlueprintLibrary::GetTileManagerSizeUncompressed()
+{
+    return FOdysseyTileManager::Get().GetSizeUncompressed();
+}
+
+int64
+UOdysseyTileManagerBlueprintLibrary::GetTileManagerNumTilesCompressed()
+{
+    return FOdysseyTileManager::Get().GetNumTilesCompressed();
+}
+
+int64
+UOdysseyTileManagerBlueprintLibrary::GetTileManagerSizeCompressed()
+{
+    return FOdysseyTileManager::Get().GetSizeCompressed();
+}
+
+int64
+UOdysseyTileManagerBlueprintLibrary::GetTileManagerNumTilesOnDisk()
+{
+    return FOdysseyTileManager::Get().GetNumTilesOnDisk();
+}
+
+int64
+UOdysseyTileManagerBlueprintLibrary::GetTileManagerSizeOnDisk()
+{
+    return FOdysseyTileManager::Get().GetSizeOnDisk();
+}
